d74b74cf8bb590e7c73d19d5c00f35cac8685429
Tarde <Tarde@DAW101>
2026-03-24 17:16:13 +0100

    3

--- Práctica Git/src/proyecto/Calculadora.java
+++ Práctica Git/src/proyecto/Calculadora.java
@@ -10,4 +10,5 @@ public class Calculadora {
 	 }
 	 public double multiplicar(double a, double b) { return a * b; }
 	 public double dividir(double a, double b) { return a / b; }
+	 public double potencia(double a, double b) { return Math.pow(a, b); }
 }
